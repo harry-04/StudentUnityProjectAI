c60b8c2d1731f35cc0e01263f4779884b95be374
harry-04 <72386526+harry-04@users.noreply.github.com>
2021-12-08 01:00:02 +0000

    a

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -8,11 +8,13 @@ public class PlayerMove : MonoBehaviour
     private Animator animator;
     private float speed;
     private Rigidbody body;
+    private Health health; //New health variable
 
 
     
     void Start()
     {
+        health = GetComponent<Health>(); // Get the health component
         animator = transform.GetChild(0).GetComponent<Animator>();
         body = GetComponent<Rigidbody>();
         speed = 1.5f;
@@ -36,5 +38,17 @@ public class PlayerMove : MonoBehaviour
         {
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movementVector), 0.25f);
         }
+
+        animator.SetBool("Walking", movementVector != Vector3.zero);
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Hurt(2, 2);
+        }
+    }
+
+    public void Hurt(int amount, int delay = 0)
+    {
+        StartCoroutine(health.TakeDamageDelayed(amount, delay));
     }
 }
